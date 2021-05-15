c97b3a6bcdf7cf75301d1433e56b8fe34525d955
Nghĩa Nef <nghia14302@gmail.com>
2021-05-15 09:43:29 +0700

    Updates

---
+++ precache-manifest.1343863a100c295655ae884a4389f4a8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6554755dbe90b43d2a56",
+    "url": "/Demo-countdown-clock/static/css/main.e27278e0.chunk.css"
+  },
+  {
+    "revision": "6554755dbe90b43d2a56",
+    "url": "/Demo-countdown-clock/static/js/main.e8e5f68e.chunk.js"
+  },
+  {
+    "revision": "ec9084528418fefeeedb",
+    "url": "/Demo-countdown-clock/static/js/runtime~main.9739c41a.js"
+  },
+  {
+    "revision": "c2eb4402eec6b650f9de",
+    "url": "/Demo-countdown-clock/static/js/2.b11e1dfa.chunk.js"
+  },
+  {
+    "revision": "7db361069cc52ba48096",
+    "url": "/Demo-countdown-clock/static/js/3.63a8b96a.chunk.js"
+  },
+  {
+    "revision": "afd18e45af4d40379d2bd8626c4e117b",
+    "url": "/Demo-countdown-clock/static/media/SVN-Genica_Pro.afd18e45.otf"
+  },
+  {
+    "revision": "1027aa7c756a49b5f4814ead28a08bc5",
+    "url": "/Demo-countdown-clock/static/media/square_sans_serif_7.1027aa7c.ttf"
+  },
+  {
+    "revision": "b5819e84923b6331557271bcd8158080",
+    "url": "/Demo-countdown-clock/index.html"
+  }
+];
